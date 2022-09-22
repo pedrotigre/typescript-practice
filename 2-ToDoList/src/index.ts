a363e93c75667915c9be17d0cc14c060c4d61e8a
Pedro Tigre <pedrogtigre@gmail.com>
2022-09-22 12:31:42 -0300

    ! approach added

--- 2-ToDoList/src/index.ts
+++ 2-ToDoList/src/index.ts
@@ -1,5 +1,5 @@
-const btn = document.querySelector('.btn');
+const btn = document.querySelector('.btn')!;
 
-btn?.addEventListener('click', () => {
+btn.addEventListener('click', () => {
   console.log('Hello!');
 });
