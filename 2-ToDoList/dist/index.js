1fab3fd4d4e3985ef4434addf6acaba607ab3d27
Pedro Tigre <pedrogtigre@gmail.com>
2022-10-17 18:13:18 -0300

    Added to do list

--- 2-ToDoList/dist/index.js
+++ 2-ToDoList/dist/index.js
@@ -1,6 +1,19 @@
 "use strict";
 const btn = document.querySelector('.btn');
 const input = document.querySelector('.toDoInput');
-btn.addEventListener('click', () => {
-    alert(input.value);
-});
+const list = document.querySelector('.toDoList');
+const form = document.querySelector('form');
+const handleSubmit = (e) => {
+    e.preventDefault();
+    const listItem = document.createElement('li');
+    const checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
+    listItem.append(input.value);
+    listItem.append(checkbox);
+    list.append(listItem);
+    input.value = '';
+};
+form.addEventListener('submit', handleSubmit);
+// btn.addEventListener('click', () => {
+//     alert(input.value);
+// });
--- 2-ToDoList/src/index.ts
+++ 2-ToDoList/src/index.ts
@@ -1,6 +1,21 @@
 const btn = document.querySelector('.btn')! as HTMLButtonElement;
 const input = document.querySelector('.toDoInput')! as HTMLInputElement;
+const list = document.querySelector('.toDoList')!;
+const form = document.querySelector('form')!;
 
-btn.addEventListener('click', () => {
-    alert(input.value);
-});
+const handleSubmit = (e: SubmitEvent) => {
+    e.preventDefault();
+    const listItem = document.createElement('li');
+    const checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
+    listItem.append(input.value);
+    listItem.append(checkbox);
+    list.append(listItem);
+    input.value = '';
+};
+
+form.addEventListener('submit', handleSubmit);
+
+// btn.addEventListener('click', () => {
+//     alert(input.value);
+// });
