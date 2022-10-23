83591ee23fb6c5c395eb91e608ee96033c606bd8
Pedro Tigre <pedrogtigre@gmail.com>
2022-10-23 13:30:47 -0300

    Added local storage integration

--- 2-ToDoList/dist/index.js
+++ 2-ToDoList/dist/index.js
@@ -3,16 +3,41 @@ const btn = document.querySelector('.btn');
 const input = document.querySelector('.toDoInput');
 const list = document.querySelector('.toDoList');
 const form = document.querySelector('form');
+const readTodos = () => {
+    const todos = localStorage.getItem('todos');
+    if (todos != null)
+        return JSON.parse(todos);
+    return [];
+};
+const todos = readTodos();
+const saveTodos = () => {
+    localStorage.setItem('todos', JSON.stringify(todos));
+};
 const handleSubmit = (e) => {
     e.preventDefault();
+    const newTodo = {
+        text: input.value,
+        completed: false,
+    };
+    todos.push(newTodo);
+    createTodo(newTodo);
+    input.value = '';
+    saveTodos();
+};
+const createTodo = (todo) => {
     const listItem = document.createElement('li');
     const checkbox = document.createElement('input');
     checkbox.type = 'checkbox';
-    listItem.append(input.value);
+    checkbox.checked = todo.completed;
+    checkbox.addEventListener('change', () => {
+        todo.completed = checkbox.checked;
+        saveTodos();
+    });
+    listItem.append(todo.text);
     listItem.append(checkbox);
     list.append(listItem);
-    input.value = '';
 };
+todos.forEach(createTodo);
 form.addEventListener('submit', handleSubmit);
 // btn.addEventListener('click', () => {
 //     alert(input.value);
--- 2-ToDoList/src/index.ts
+++ 2-ToDoList/src/index.ts
@@ -3,16 +3,51 @@ const input = document.querySelector('.toDoInput')! as HTMLInputElement;
 const list = document.querySelector('.toDoList')!;
 const form = document.querySelector('form')!;
 
+interface Todo {
+    text: string;
+    completed: boolean;
+}
+
+const readTodos = () => {
+    const todos = localStorage.getItem('todos');
+    if (todos != null) return JSON.parse(todos);
+    return [];
+};
+
+const todos: Todo[] = readTodos();
+
+const saveTodos = () => {
+    localStorage.setItem('todos', JSON.stringify(todos));
+};
+
 const handleSubmit = (e: SubmitEvent) => {
     e.preventDefault();
+    const newTodo: Todo = {
+        text: input.value,
+        completed: false,
+    };
+    todos.push(newTodo);
+    createTodo(newTodo);
+    input.value = '';
+    saveTodos();
+};
+
+const createTodo = (todo: Todo) => {
     const listItem = document.createElement('li');
     const checkbox = document.createElement('input');
     checkbox.type = 'checkbox';
-    listItem.append(input.value);
+    checkbox.checked = todo.completed;
+    checkbox.addEventListener('change', () => {
+        todo.completed = checkbox.checked;
+        saveTodos();
+    });
+
+    listItem.append(todo.text);
     listItem.append(checkbox);
     list.append(listItem);
-    input.value = '';
 };
+
+todos.forEach(createTodo);
 
 form.addEventListener('submit', handleSubmit);
 
